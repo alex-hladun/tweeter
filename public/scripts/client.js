fc4a93219aae346cdab670b8b3cf11b5ca88c3be
alex-hladun <alexhladun@gmail.com>
2020-06-16 23:31:03 +0000

    All responsive elements now work.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,44 +10,39 @@ $(document).ready(function () {
 
   $("#submit-tweet-form").submit(function (event) {
     event.preventDefault();
-
     const payload = $(this).serialize();
     const tweetToPost = $('#tweet-text').val();
     if (tweetToPost.length === 0) {
-      $("#error-message").text("Dis tweet empty!");
-      $("#error-message").slideDown("slow", function() {
+      $("#error-message").text("Your tweet is empty!");
+      $("#error-message").slideDown("slow", function () {
       });
-      // window.alert("This tweet is empty");
     } else if (tweetToPost.length > 140) {
-      $("#error-message").text("Dis tweet too long!");
-      $("#error-message").slideDown("slow", function() {
+      $("#error-message").text("Your tweet is too long!");
+      $("#error-message").slideDown("slow", function () {
       });
-      // window.alert("This tweet is too long!");
     } else {
       $.ajax({
         type: "POST",
         url: '/tweets',
         data: payload,
       })
-        .then(function(data) {
-          $("#error-message").slideUp("slow", function() {
+        .then(function () {
+          $("#error-message").slideUp("slow", function () {
           });
           $('#tweet-text').val("");
-          console.log("Data posted to tweet database.");
-          console.log(data);
+          $('#char-counter').val('140');
           loadLatestTweet();
         });
-
     }
   }
   );
 
   // FAB button
-  $('button.back-to-tweet-compose').click(function(event) {
+  $('button.back-to-tweet-compose').click(function () {
     console.log("FAB clicked");
-    $("#compose-tweet-form").slideDown("slow", function() {
+    $("#compose-tweet-form").slideDown("slow", function () {
     });
-    $("#error-message").slideUp("slow", function() {
+    $("#error-message").slideUp("slow", function () {
     });
     $("#tweet-text").focus();
   });
@@ -55,66 +50,69 @@ $(document).ready(function () {
   // Slide-down for compose tweet.
   $("a.write-text").click(function (event) {
     event.preventDefault();
-    $("#compose-tweet-form").slideToggle("slow", function() {
+    $("#compose-tweet-form").slideToggle("slow", function () {
     });
-    $("#error-message").slideUp("slow", function() {
+    $("#error-message").slideUp("slow", function () {
     });
     $("#tweet-text").focus();
   });
-  
 
   // Toggle FAB if not in desktop mode.
-  $(window).on('resize', function() {
-    console.log($(window).width());
-    if ($(window).width() < 1024) {
+  specifyFab();
 
-      const amountScrolled = 330;
-      $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-          $('button.back-to-tweet-compose').addClass("show");
-          $('a.write-text').addClass("hide");
-        } else {
-          $('button.back-to-tweet-compose').removeClass("show");
-          $('a.write-text').removeClass("hide");
-        }
-      });
-    } else {
-      // Need to un-bind
-      $(window).off('scroll');
-    }
+  $(window).on('resize', function () {
+    specifyFab();
   });
-
-
-// $('button.back-to-top').click(function() {
-//   $('html, body').animate({
-//     scrollTop: 0
-//   }, 800);
-//   return false;
-// });
-
-
-
-
+  $(window).scroll(function () {
+    console.log('scroll');
+    specifyFab();
+  });
 });
 
+const specifyFab = () => {
+  if ($(window).width() < 1024) {
+    const amountScrolled = 315;
+    if ($(window).scrollTop() > amountScrolled) {
+      // FAB
+      $('button.back-to-tweet-compose').addClass("show");
+      // Text
+      $('a.write-text').addClass("hide");
+      $('#tweeter-text').addClass('float');
+    } else {
+      $('button.back-to-tweet-compose').removeClass("show");
+      $('a.write-text').removeClass("hide");
+      $('#tweeter-text').removeClass('float');
+    }
+  } else {
+    $('#tweeter-text').removeClass('float');
+    $('button.back-to-tweet-compose').removeClass("show");
+    $('a.write-text').removeClass("hide");
+  }
+};
+
 const createTweetElement = (tweetData) => {
-
-  
-
-  
-
-  let date1 = new Date();
-  let date2 = tweetData.created_at;
-  let differenceInTime = date1.getTime() - date2;
-  let differenceInDays = (differenceInTime / (1000 * 2600 * 24)).toFixed(0);
+  const date1 = new Date();
+  const date2 = tweetData.created_at;
+  const differenceInTime = date1.getTime() - date2;
+  const differenceInDays = (differenceInTime / (1000 * 3600 * 24));
   const differenceInYears = Math.floor(differenceInDays / 365);
+  const differenceInHours = (differenceInDays * 24);
+  const differenceInMinutes = (differenceInHours * 60);
+  const differenceInSeconds = (differenceInMinutes * 60);
   let dateStr;
   if (differenceInDays > 365) {
-    dateStr = differenceInYears + " years ago";
+    dateStr = differenceInYears.toFixed(0) + " years ago";
+  } else if (differenceInSeconds < 60) {
+    dateStr = differenceInSeconds.toFixed(0) + " seconds ago";
+  } else if (differenceInMinutes < 60) {
+    dateStr = differenceInMinutes.toFixed(0) + " minutes ago";
+  } else if (differenceInHours < 24) {
+    dateStr = differenceInHours.toFixed(0) + " hours ago";
   } else {
-    dateStr = differenceInDays + " days ago";
+    dateStr = differenceInDays.toFixed(0) + " days ago";
   }
-  console.log("diff in days", differenceInDays);
+
+  console.log("datestr: ", dateStr);
   const $tweet = $(`<article class="tweet">
   <header>
     <div class="tweet-info-group">
@@ -126,22 +124,28 @@ const createTweetElement = (tweetData) => {
     </div>
   </header>
   <section>
-    <span>${escape(tweetData.content.text)}</span>
+    <span class="tweet">${escape(tweetData.content.text)}</span>
   </section>
   <footer>
     <div class="tweet-info-group">
-      <span>${`${dateStr}`}</span>
+      <span>${dateStr}</span>
     </div>
     <div class="tweetinfo-group">
       <span class="symbols">❤︎ ⚐ ♺</span>
-
   </footer>
 </article>`);
   return $tweet;
 };
 
+const applyUsernameHover = () => {
+  $("article.tweet").hover(function () {
+    $(this).find('.username').show();
+  }, function () {
+    $(this).find('.username').hide();
+  });
+};
+
 const renderTweets = (tweets) => {
-  console.log(tweets);
   for (const tweet of tweets) {
     const $tweet = createTweetElement(tweet);
     $('#tweet-container').append($tweet);
@@ -157,18 +161,11 @@ const loadTweets = () => {
         tweetArray.push(obj);
       }
       renderTweets(tweetArray);
-
-      $("article.tweet").hover(function () {
-        let username = $(this).children('header').children()[1];
-        $(username).toggle();
-      }, function () {
-        let username = $(this).children('header').children()[1];
-        $(username).toggle();
-      });
+      applyUsernameHover();
     });
 };
 
-const escape =  function(str) {
+const escape = function (str) {
   let div = document.createElement('span');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
@@ -179,10 +176,6 @@ const loadLatestTweet = () => {
     .then(function (data) {
       console.log('Success: ', data);
       const tweetArray = [];
-
-      // const keys = Object.keys(data);
-      // const last = keys[keys.length-1];
-
       let x = 0;
       for (const obj of data) {
         if (x === data.length - 1) {
@@ -190,19 +183,7 @@ const loadLatestTweet = () => {
         }
         x++;
       }
-
       renderTweets(tweetArray);
-
-      $("article.tweet").hover(function () {
-        let newusername = $(this).children('header').children()[1];
-        $(newusername).toggle();
-      }, function () {
-        let newusername = $(this).children('header').children()[1];
-        $(newusername).toggle();
-      });
+      applyUsernameHover();
     });
 };
-
-
-
-
